7106ed474ce1fa5f7d4b5c720051a0936644cee4
moonhyerin <61622127+moonhyerin@users.noreply.github.com>
2021-01-12 17:07:28 +0900

    210112 라이프사이클까지 완료

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
 import ScrollBox from "./ScrollBox";
 import IterationSample from "./IterationSample";
-//집에서 push 해보기 테스트
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
+
+//랜덤 색상 생성하기
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+  
+  //클릭 시 랜덤색상이 this.state.color로 설정된다.
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
       <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-        <hr/>
-        <IterationSample />
+        <ErrorBoundary>
+          <button onClick={this.handleClick}>랜덤 색상</button>
+          <LifeCycleSample color={this.state.color}/>
+        </ErrorBoundary>
       </div>
     );
   }
--- src/LifeCycleSample.js
+++ src/LifeCycleSample.js
@@ -13,7 +13,7 @@ class LifeCycleSample extends Component {
         console.log('constructor');
     }
 
-    //props로 받아온 값을 state로 동기화 할 때 사용
+    //props로 받아온 값을 state로 동기화 할 때 사용 ==> 부모 컴포넌트에서 전달 받은 color를 이 컴포넌트의 state로 설정
     static getDerivedStateFromProps(nextProps, prevState) {
         console.log('getDerivedStateFromProps');
         if(nextProps.color !== prevState.color) {
@@ -29,7 +29,7 @@ class LifeCycleSample extends Component {
 
     shouldComponentUpdate(nextProps, nextState) {
         console.log('shouldComponentUpdate', nextProps, nextState);
-        //숫자의 마지막 자리가 4면 리렌더링 하지 않는다.
+        //숫자의 마지막 자리가 4면 리렌더링 하지 않는다. (false 반환)
         return nextState.number % 10 !== 4;
     }
 
@@ -43,6 +43,7 @@ class LifeCycleSample extends Component {
         })
     }
 
+    //DOM에 변화가 일어나기 직전의 색상 속성을 snapshot 값으로 반환하여 componentDidUpdate에서 조회할 수 있게
     getSnapshotBeforeUpdate(prevProps, prevState) {
         console.log('getSnapshotBeforeUpdate');
         if(prevProps.color !== this.props.color) {
@@ -50,13 +51,31 @@ class LifeCycleSample extends Component {
         }
         return null;
     }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('componentDidUpdate', prevProps, prevState);
+        if(snapshot) {
+            console.log('업데이트되기 직전 색상: ' + snapshot);
+        }
+    }
     
     render() {
         console.log('render');
 
+        const style = {
+            color: this.props.color
+        }
+
         return (
             <div>
-                
+                {this.props.missing.value}
+                <h1 style={style} ref={ref => this.myRef=ref}>
+                    {this.state.number}
+                </h1>
+                <p>color: {this.state.color}</p>
+                <button onClick={this.handleClick}>
+                    더하기
+                </button>
             </div>
         );
     }
